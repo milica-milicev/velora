76adec932a046b8843e578b34536065b39f16e2b
milica-milicev <142879244+milica-milicev@users.noreply.github.com>
2024-11-25 15:38:36 +0100

    404 updated

--- wp-content/themes/velora/404.php
+++ wp-content/themes/velora/404.php
@@ -11,17 +11,35 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-		<div class="container">
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'nm_theme' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'nm_theme' ); ?></p>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</div>
+		<section class="error-404 not-found">
+			<div class="error-404__header">
+				<div class="container">
+					<h1 class="error-404__title">
+						<span class="error-404__title-1">4</span>
+						<span class="error-404__title-2">0</span>
+						<span class="error-404__title-3">4</span>
+					</h1>
+				</div>
+			</div>
+
+			<div class="error-404__content">
+				<div class="container">
+					<div class="error-404__text">
+						<p><?php esc_html_e( 'Poštovani korisniče,', 'nm_theme' ); ?></p>
+						<p><?php esc_html_e( 'Stranica ili proizvod koji tražite je trenutno nedostupan.', 'nm_theme' ); ?></p>
+					</div>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="error-404__btn btn">Povratak na početnu stranicu</a>
+				</div>
+			</div><!-- .page-content -->
+
+			<div class="error-404__tiles">
+				<div class="container">
+					<?php get_template_part( 'template-views/blocks/tiles/tiles' ); ?>
+				</div>
+			</div>
+		</section><!-- .error-404 -->
+
 	</main><!-- #main -->
 
 <?php
--- wp-content/themes/velora/woocommerce/order/order-details-customer.php
+++ wp-content/themes/velora/woocommerce/order/order-details-customer.php
@@ -47,7 +47,7 @@ $show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_a
 		</div><!-- /.col-1 -->
 
 		<div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
-			<h2 class="woocommerce-column__title"><?php esc_html_e( 'Shipping address', 'woocommerce' ); ?></h2>
+			<h2 class="woocommerce-column__title order-details__title"><?php esc_html_e( 'Shipping address', 'woocommerce' ); ?></h2>
 			<address>
 				<?php echo wp_kses_post( $order->get_formatted_shipping_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
 
